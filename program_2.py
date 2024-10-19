cd98b3312829be32dd9f17a23e39208201e175a2
The-R0nin <2025esteele@isd2144.org>
2024-10-18 19:19:24 -0500

    Update program_2.py
    Ethan Steele
    10/18
    Almost there

--- program_2.py
+++ program_2.py
@@ -19,64 +19,21 @@ def main():
     # Display the list of numbers that are larger
     # than the number.
     print(f'List of numbers that are larger than {number}:')
-    print(number < number_list[0])
-    if number_list[0] == False:
-        number_list.remove(number_list[0])
-
-    print(number < number_list[1])
-    if number_list[1] == False:
-        number_list.remove(number_list[1])
-
-    print(number < number_list[2])
-    if number_list[2] == False:
-        number_list.remove(number_list[2])
-
-    print(number < number_list[3])
-    if number_list[3] == False:
-        number_list.remove(number_list[3])
-
-    print(number < number_list[4])
-    if number_list[4] == False:
-        number_list.remove(number_list[4])
-
-    print(number < number_list[5])
-    if number_list[5] == False:
-        number_list.remove(number_list[5])
-
-    print(number < number_list[6])
-    if number_list[6] == False:
-        number_list.remove(number_list[6])
-
-    print(number < number_list[7])
-    if number_list[7] == False:
-        number_list.remove(number_list[7])
-
-    print(number < number_list[8])
-    if number_list[8] == False:
-        number_list.remove(number_list[8])
-
-    print(number < number_list[9])
-    if number_list[9] == False:
-        number_list.remove(number_list[9])
-
-    print(f'{number_list}')
+    a = 0
+    while a < len(number_list):
+        print(number < number_list[a])
+        a += 1
+    
     # Call the display_larger_than_n_list function,
     # passing a number and number list as arguments.
 
     display_larger_than_n_list(number, number_list)
     n = int(input('Number: '))
-    n_list = number_list   
-    print(n < n_list[0])
-    print(n < n_list[1])
-    print(n < n_list[2])
-    print(n < n_list[3])
-    print(n < n_list[4])
-    print(n < n_list[5])
-    print(n < n_list[6])
-    print(n < n_list[7])
-    print(n < n_list[8])
-    print(n < n_list[9])
-    print(f'{n_list}')
+    n_list = number_list
+    u = 0
+    while u < len(n_list):
+        print(n < n_list[u])
+        u += 1
 
 # The display_larger_than_n_list function accepts two arguments:
 # a list, and a number. The function displays all of the numbers
@@ -90,4 +47,5 @@ def display_larger_than_n_list(n, n_list):
 
 if __name__ == '__main__':
     main()
+
 
